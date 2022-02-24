1786b3aee4385fbc5c88be21ab77caaa3d089bce
Luke Appleton <luke.d.appleton@gmail.com>
2022-02-24 10:22:39 -0700

    Updated tf file for security group

--- part_two/reference.tf
+++ part_two/reference.tf
@@ -26,3 +26,13 @@ resource "aws_eip_association" "eip_assoc" {
     instance_id = aws_instance.myec2.id
     allocation_id = aws_eip.lb.id
 }
+
+resource "aws_security_group" "allow_tls" {
+    name = "tf-class_group"
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = "${aws_eip.lb.public_ip}/32"
+    }
+}
