c2a56e055e8cb936f68dd69989670ba9d3bf08e3
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 11:14:37 -0700

    Removed content from old dynamic ingress code

--- part_three/securitygroup.tf
+++ part_three/securitygroup.tf
@@ -2,10 +2,8 @@ resource "aws_security_group" "allow_ssh" {
     name = "tf-class_group"
 
     ingress {
-        content {
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-        }
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
     }
 }
