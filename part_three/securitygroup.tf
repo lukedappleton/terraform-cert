fb92b00466241392b8c77886ba6172e176d941a0
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 11:36:29 -0700

    Added resource to attach security group to ec2 instance

--- part_three/securitygroup.tf
+++ part_three/securitygroup.tf
@@ -7,3 +7,8 @@ resource "aws_security_group" "allow_ssh" {
         protocol = "tcp"
     }
 }
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.allow_ssh.id
+  network_interface_id = aws_instance.myec2.primary_network_interface_id
+}
