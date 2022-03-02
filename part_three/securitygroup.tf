ab1da169a3e4a3e4859d081718d21ff2d9e01d18
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 12:07:17 -0700

    Added cidr block arg

--- part_three/securitygroup.tf
+++ part_three/securitygroup.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "allow_ssh" {
     name = "tf-class_group"
 
     ingress {
+        cidr_blocks = [ "0.0.0.0/0", ]
         from_port = 22
         to_port = 22
         protocol = "tcp"
