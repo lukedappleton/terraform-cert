54e1665e1df6c32e57c591ccf424bfb458ad2116
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 08:57:24 -0700

    Added an iterator to dynamic block

--- part_two/securitygroup.tf
+++ part_two/securitygroup.tf
@@ -2,11 +2,12 @@ resource "aws_security_group" "allow_tls" {
     name = "tf-class_group"
     dynamic "ingress" {
         for_each = var.sg_ports
-        content = {
-        from_port = ingress.value
-        to_port = ingress.value
-        protocol = "tcp"
-        cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+        iterator = port
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "tcp"
+            cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
         }
     }
 }
