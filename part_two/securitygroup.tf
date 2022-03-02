846dfd5884a1e7d93c311cc60bd2a0a9db81f2e3
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 09:03:05 -0700

    Added egress rules

--- part_two/securitygroup.tf
+++ part_two/securitygroup.tf
@@ -1,6 +1,18 @@
 resource "aws_security_group" "allow_tls" {
     name = "tf-class_group"
+
     dynamic "ingress" {
+        for_each = var.sg_ports
+        iterator = port
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "tcp"
+            cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+        }
+    }
+
+        dynamic "egress" {
         for_each = var.sg_ports
         iterator = port
         content {
