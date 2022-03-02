386ae1f5fd2988d1aefc48b62afd9b8fb8b68b93
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 11:13:32 -0700

    Removed dynamic ingress in the security group

--- part_three/securitygroup.tf
+++ part_three/securitygroup.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_ssh" {
     name = "tf-class_group"
 
-    dynamic "ingress" {
+    ingress {
         content {
             from_port = 22
             to_port = 22
