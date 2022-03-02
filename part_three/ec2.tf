63e2f01e395b10c9762ef61b083b0aa46f8f3260
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 12:54:43 -0700

    Added outbound security rule

--- part_three/ec2.tf
+++ part_three/ec2.tf
@@ -16,8 +16,7 @@ resource "aws_instance" "myec2" {
 
   provisioner "remote-exec" {
       inline = [
-          "sudo yum update -y",
-          "sudo yum install -y nginx",
+          "sudo amazon-linux-extras install -y nginx",
           "sudo systemctl start nginx"
       ]
   }
--- part_three/securitygroup.tf
+++ part_three/securitygroup.tf
@@ -7,4 +7,11 @@ resource "aws_security_group" "allow_ssh" {
         to_port = 22
         protocol = "tcp"
     }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
