bf36d8e71595a6a895e5078a3b86b0829b2e4267
Luke Appleton <luke.d.appleton@gmail.com>
2022-03-02 08:54:47 -0700

    Added dynamic block to security group

--- part_two/securitygroup.tf
+++ part_two/securitygroup.tf
@@ -1,9 +1,12 @@
 resource "aws_security_group" "allow_tls" {
     name = "tf-class_group"
-    ingress {
-        from_port = 443
-        to_port = 443
+    dynamic "ingress" {
+        for_each = var.sg_ports
+        content = {
+        from_port = ingress.value
+        to_port = ingress.value
         protocol = "tcp"
         cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+        }
     }
 }
--- part_two/variables.tf
+++ part_two/variables.tf
@@ -10,3 +10,9 @@ variable "elb_names" {
     type = list
     default = ["dev-loadbalancer", "stage-loadbalancer", "prod-loadbalancer"]
 }
+
+variable "sg_ports" {
+  type = list(number)
+  description = "list of ingress ports"
+  default = [8200, 8201, 8300, 9200, 9500]
+}
